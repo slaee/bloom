b215ff4750d15d2f6d5c776ce4cfd6b7697255a8
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-12-25 11:46:44 +0800

    Delete unecessary files

--- javascript/javascript/examples/Continue.js
+++
@@ -1,6 +0,0 @@
-// https://github.com/antlr/grammars-v4/issues/3772
-for (var i = 0; i < [].length; i++) {
-    var o = {}
-    if (false) continue
-    o[test]
-}
--- javascript/javascript/examples/LineContinuation.js
+++
@@ -1,3 +0,0 @@
-var sql = "line 1 \
-	some other line \
-the last line"
